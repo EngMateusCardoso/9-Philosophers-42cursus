b933681eb7f96dafca77605f42693db1788b8cc1
EngMateusCardoso <mateusoliver490@gmail.com>
2023-04-29 11:40:36 -0300

    fix deadlocks

--- philo/src/philo_life.c
+++ philo/src/philo_life.c
@@ -6,7 +6,7 @@
 /*   By: matcardo <matcardo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 12:29:11 by matcardo          #+#    #+#             */
-/*   Updated: 2023/04/16 17:52:12 by matcardo         ###   ########.fr       */
+/*   Updated: 2023/04/29 11:37:01 by matcardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,16 @@
 
 void	take_forks(t_philo *philo)
 {
-	pthread_mutex_lock(philo->right_fork);
-	pthread_mutex_lock(philo->left_fork);
+	if (philo->id % 2 == 0 && philo->id != philo->philo_env->philo_nbr)
+	{
+		pthread_mutex_lock(philo->left_fork);
+		pthread_mutex_lock(philo->right_fork);
+	}
+	else
+	{
+		pthread_mutex_lock(philo->right_fork);
+		pthread_mutex_lock(philo->left_fork);
+	}
 	pthread_mutex_lock(&(philo->philo_env->lock_print));
 	printf("%d has taken a fork\n", philo->id);
 	pthread_mutex_unlock(&(philo->philo_env->lock_print));
@@ -29,12 +37,12 @@ void	eating(t_philo *philo)
 	usleep(philo->philo_env->time_to_eat * 1000);
 	// philo->last_eat = get_time();
 	philo->eat_count++;
+	pthread_mutex_unlock(philo->right_fork);
+	pthread_mutex_unlock(philo->left_fork);
 }
 
 void	sleeping(t_philo *philo)
 {
-	pthread_mutex_unlock(philo->right_fork);
-	pthread_mutex_unlock(philo->left_fork);
 	pthread_mutex_lock(&(philo->philo_env->lock_print));
 	printf("%d is sleeping\n", philo->id);
 	pthread_mutex_unlock(&(philo->philo_env->lock_print));
